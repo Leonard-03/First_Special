f70b9a2f313befad88f2e58d7270b90971eb1977
Leonard-03 <leonardpeloton@gmail.com>
2025-03-01 00:26:23 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function message()
+{
+    alert("Every moment with you feels like a beautiful dream I never want to wake up from. You are my heart’s greatest joy, my soul’s deepest peace, and the love I never knew I needed so much. No matter where life takes us, my heart will always find its way back to you. I love you endlessly.");
+}
+
